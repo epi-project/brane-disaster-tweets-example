c26c1bfcefd1c021fbe2281cb4cee75180298634
damon wang <wangjycode@gmail.com>
2022-05-31 08:19:56 +0200

    add docment

--- packages/visualization/run.py
+++ packages/visualization/run.py
@@ -8,22 +8,37 @@ import yaml
 import codecs
 from visualization import *
 
-def visualization_action(input: any):
-    predict_data = pd.read_csv(input+'submission.csv')
-    test_data = pd.read_csv(input+'test.csv')
+def visualization_action(filepath_test_dataset,filepath_sub_dataset):
+    """
+    Create an Html that contains all the plots based on the test and submission datasets.
 
-    prdict_data = pd.merge(predict_data, test_data, on="id")
-    location_img = location_profile(prdict_data)
-    keywords_word_cloud_img = keywords_word_cloud(prdict_data)
-    keywords_polt_img = keywords_polt(prdict_data)
-    tweets_imgs = tweets_profile(prdict_data)
+    Parameters
+    ----------
+    filepath_test_dataset: `str`
+    CSV file containing the test dataset.
+
+    filepath_sub_dataset: `str`
+    CSV file containing the submission dataset.
+
+    Returns
+    -------
+    `str` The result of the process.
+    """
+    
+    sub_data = pd.read_csv(filepath_test_dataset)
+    test_data = pd.read_csv(filepath_sub_dataset)
+
+    predict_data = pd.merge(sub_data, test_data, on="id")
+    location_img = location_profile(predict_data)
+    keywords_word_cloud_img = keywords_word_cloud(predict_data)
+    keywords_polt_img = keywords_polt(predict_data)
+    tweets_imgs = tweets_profile(predict_data)
     prdict_img = prediction_plot(predict_data)
 
     template_html = codecs.open("./result.html","r","utf-8")
     
     result = template_html.read().format(prediction_overview=prdict_img,keywords_word_cloud=keywords_word_cloud_img,keywords_top30=keywords_polt_img,tweets_text_word_cloud=tweets_imgs[1],tweets_text_word_frequency_top30=tweets_imgs[0],disaster_location_top10=location_img)
     
-    # We wrap the writing in a try/catch so we may catch any errors
     try:
         # Open the file and write the content
         with open(f"/data/result.html", "w") as f:
@@ -32,11 +47,11 @@ def visualization_action(input: any):
         return "success"
     # Catch file errors
     except IOError as e:
-        # Return the non-zero exit code that they define
         return e.errno
 
 
 if __name__ == '__main__':
-    input = os.environ["INPUT"]
-    output = visualization_action(input)
+    filepath_test_dataset = os.environ["FILEPATH_TEST_DATASET"]
+    filepath_sub_dataset = os.environ["FILEPATH_SUB_DATASET"]
+    output = visualization_action(filepath_test_dataset,filepath_sub_dataset)
     print(yaml.dump({"output": output}))
