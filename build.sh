392b7874ca78a097917c9acc6ccbad6a3e4274d3
damon wang <wangjycode@gmail.com>
2022-05-31 21:23:46 +0200

    add output and exit

--- build.sh
+++ build.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
-echo "---- Start Build ----"
 
-target=$0
+target=$1
 
 if [ ! -d "packages/compute" ]; then
   echo "Please enter the root of the project, then re-run the script!"
   exit 1
 fi
-echo "$targe"
 
 if [[ $target == "compute" ]]; then
+    echo "---- Start Build The Compute Package ----"
     cd packages/compute
     brane build ./container.yml 
 
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build compute package failed, please check the error message!"
+        exit 1
     fi
 elif [[ $target == "visualization" ]]; then
+    echo "---- Start Build The Visualization Package ----"
     cd packages/visualization
     brane build ./container.yml
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build visualization package failed, please check the error message!"
+        exit 1
     fi
 elif [[ $target == "utils" ]]; then
+    echo "---- Start Build The Utils Package ----"
     cd packages/utils
     brane build ./container.yml 
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build utils package failed, please check the error message!"
+        exit 1
     fi
 else
+    echo "---- Start Build All The Package ----"
     cd packages/compute
     brane build ./container.yml 
 
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build compute package failed, please check the error message!"
+        exit 1
     fi
 
 
@@ -47,6 +53,7 @@ else
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build utils package failed, please check the error message!"
+        exit 1
     fi
 
     cd ../visualization
@@ -54,7 +61,9 @@ else
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Build visualization package failed, please check the error message!"
+        exit 1
     fi
 fi
 
 echo "---- End Build ----"
+exit 0
