0f53f6c42cb2e53ce41d2ff22998f34cb6d5605d
damon wang <wangjycode@gmail.com>
2022-06-02 23:55:17 +0200

    change util test

--- packages/utils/tests/test_util.py
+++ packages/utils/tests/test_util.py
@@ -1,25 +1,29 @@
-
-from asyncore import write
 import unittest
 import os
 from ..run import download
 from unittest import mock
-
+# save the unpatched versions of the mocked functions
 builtin_open = open
-file_mock = unittest.mock.Mock(write=unittest.mock.Mock())
-open_mock = unittest.mock.Mock(return_value=file_mock)  
-    
+
+def mock_open(*args, **kwargs):
+    _args = [*args]
+    _args[0] = str(args[0]).replace("/data/", "")
+    return builtin_open(*_args, **kwargs)
+
 class TestUtils(unittest.TestCase):
-    
+    @classmethod
+    def tearDownClass(self):
+        os.remove("test.csv")
+
+    @mock.patch("builtins.open", mock_open)
     def test_download(self):
         repo_owner = "marinoandrea";
         repo_name = "disaster-tweets-brane";
         repo_dataset_filepath_test = "data/test.csv";
 
         result = download(repo_owner, repo_name, repo_dataset_filepath_test, "test.csv")
-        assert(result != 0)
-        
-
+        assert(result == 0)
+        assert(os.path.exists("test.csv"))
         
 if __name__ == '__main__':
     unittest.main()
